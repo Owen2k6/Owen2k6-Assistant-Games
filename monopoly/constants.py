4d17ee3114f85411c9d070670c5e521c88b001b0
Owen2k6 <fulptdm@gmail.com>
2022-11-28 18:54:00 +0000

    Fixes

--- monopoly/constants.py
+++ monopoly/constants.py
@@ -74,46 +74,46 @@ RENTPRICE = [
 ]
 RRPRICE = [0, 25, 50, 100, 200]
 CCNAME = [
-	'Advance to Go (Collect $200)',
-	'Bank error in your favor\nCollect $200',
-	'Doctor\'s fee\nPay $50',
-	'From sale of stock you get $50',
-	'Get Out of Jail Free',
-	'Go to Jail\nGo directly to jail\nDo not pass Go\nDo not collect $200',
-	'Grand Opera Night\nCollect $50 from every player for opening night seats',
-	'Holiday Fund matures\nReceive $100',
-	'Income tax refund\nCollect $20',
-	'It is your birthday\nCollect $10',
-	'Life insurance matures\nCollect $100',
-	'Pay hospital fees of $100',
-	'Pay school fees of $150',
-	'Receive $25 consultancy fee',
-	'You are assessed for street repairs\n$40 per house\n$115 per hotel',
-	'You have won second prize in a beauty contest\nCollect $10',
-	'You inherit $100'
+	'> Advance to Go (Collect $200)',
+	'> Bank error in your favor\nCollect $200',
+	'> Doctor\'s fee\nPay $50',
+	'> From sale of stock you get $50',
+	'> Get Out of Jail Free',
+	'> Go to Jail\nGo directly to jail\nDo not pass Go\nDo not collect $200',
+	'> Grand Opera Night\nCollect $50 from every player for opening night seats',
+	'> Holiday Fund matures\nReceive $100',
+	'> Income tax refund\nCollect $20',
+	'> It is your birthday\nCollect $10',
+	'> Life insurance matures\nCollect $100',
+	'> Pay hospital fees of $100',
+	'> Pay school fees of $150',
+	'> Receive $25 consultancy fee',
+	'> You are assessed for street repairs\n$40 per house\n$115 per hotel',
+	'> You have won second prize in a beauty contest\nCollect $10',
+	'> You inherit $100'
 ]
 CHANCENAME = [
-	'Advance to Go (Collect $200)',
-	'Advance to Illinois Ave\nIf you pass Go, collect $200.',
-	'Advance to St. Charles Place\nIf you pass Go, collect $200',
+	'> Advance to Go (Collect $200)',
+	'> Advance to Illinois Ave\nIf you pass Go, collect $200.',
+	'> Advance to St. Charles Place\nIf you pass Go, collect $200',
 	(
-		'Advance token to nearest Utility. If unowned, you may buy it from the Bank. '
-		'If owned, throw dice and pay owner a total ten times the amount thrown.'
+		'> Advance token to nearest Utility. If unowned, you may buy it from the Bank. '
+		'> If owned, throw dice and pay owner a total ten times the amount thrown.'
 	), (
-		'Advance token to the nearest Railroad and pay owner twice the rental to which '
-		'he/she is otherwise entitled. If Railroad is unowned, you may buy it from the Bank.'
+		'> Advance token to the nearest Railroad and pay owner twice the rental to which '
+		'> he/she is otherwise entitled. If Railroad is unowned, you may buy it from the Bank.'
 	),
-	'Bank pays you dividend of $50',
-	'Get Out of Jail Free',
-	'Go Back 3 Spaces',
-	'Go to Jail\nGo directly to Jail\nDo not pass Go\nDo not collect $200',
-	'Make general repairs on all your property\nFor each house pay $25\nFor each hotel $100',
-	'Pay poor tax of $15',
-	'Take a trip to Reading Railroad\nIf you pass Go, collect $200',
-	'Take a walk on the Boardwalk\nAdvance token to Boardwalk',
-	'You have been elected Chairman of the Board\nPay each player $50',
-	'Your building and loan matures\nCollect $150',
-	'You have won a crossword competition\nCollect $100'
+	'> Bank pays you dividend of $50',
+	'> Get Out of Jail Free',
+	'> Go Back 3 Spaces',
+	'> Go to Jail\n> Go directly to Jail\n> Do not pass Go\n> Do not collect $200',
+	'> Make general repairs on all your property\nFor each house pay $25\nFor each hotel $100',
+	'> Pay poor tax of $15',
+	'> Take a trip to Reading Railroad\nIf you pass Go, collect $200',
+	'> Take a walk on the Boardwalk\nAdvance token to Boardwalk',
+	'> You have been elected Chairman of the Board\nPay each player $50',
+	'> Your building and loan matures\nCollect $150',
+	'> You have won a crossword competition\nCollect $100'
 ]
 MORTGAGEPRICE = [
 	-1, 30, -1, 30, -1,
